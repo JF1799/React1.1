6ffb444f569d782bf099ff8e10d92d929124d59e
Jeff Molina <jeffersogmb7@gmail.com>
2020-10-08 18:12:45 -0600

    Updates

---
+++ precache-manifest.12512ae51ccbf43a42516f64f9c5e99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4245a6721d4fedcdcabc07bedb532608",
+    "url": "/JF1799/React1.1/index.html"
+  },
+  {
+    "revision": "c8e0bd4f09081cef1d43",
+    "url": "/JF1799/React1.1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "63a996aeb992fc37529d",
+    "url": "/JF1799/React1.1/static/js/2.69f0aea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JF1799/React1.1/static/js/2.69f0aea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e0bd4f09081cef1d43",
+    "url": "/JF1799/React1.1/static/js/main.4fbcb4d5.chunk.js"
+  },
+  {
+    "revision": "cf76fb605824b107a830",
+    "url": "/JF1799/React1.1/static/js/runtime-main.e2bb4bf2.js"
+  }
+]);
